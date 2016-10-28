0ca964c930ce477136fb43c4ce51493dcf16f2b0
coillman <jequier.emmanuel@gmail.com>
2016-10-28 10:05:59 +0200

    Revert "commit test, deplacment en cours"
    This reverts commit 93fd289ca9c78f21a6d4bb6ffb1e099b00c63045.

--- src/Model/Table/FightersTable.php
+++ src/Model/Table/FightersTable.php
@@ -1,47 +1,15 @@
 <?php
-
-
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
-use Cake\ORM\TableRegistry;
 
-class FightersTable extends Table {
-
-    public function getBestFighter() {
-        return $this->find('all')->order('level')->first();
-    }
-
-    public function getFighters() {
-        return $this->find('all')->toArray();
-    }
-
-    public function move($dir, $playerid, $fighterid) {
-        $dirToCo = array();
-        switch ($dir){
-            case 'up':
-                $dirToCo = array("x"=>0, "y"=>-1);
-                break;
-            case 'down':
-                $dirToCo = array("x"=>0, "y"=>1);
-                break;
-            case 'left':
-                $dirToCo = array("x"=>-1, "y"=>0);
-                break;
-            case 'right':
-                $dirToCo = array("x"=>1, "y"=>0);
-                break;
+class FightersTable extends Table
+{
+        public function getBestFighter(){
+            return $this->find('all')->order('level')->first();
         }
         
-        $fightersTable = TableRegistry::get('Fighters');
-        $fighter = $fightersTable->get($fighterid);//ici on utilise fighterid en tant que clé
-
-        $nextPos = array('x'=>$fighter->coordinate_x += $dirToCo["x"], 'y'=>$fighter->coordinate_y += $dirToCo["y"]);
-        
-        $fighter->coordinate_x = $nextPos["x"];
-        $fighter->coordinate_y = $nextPos["y"];
-        $fightersTable->save($fighter);
+        public function getFighters(){
+            return $this->find('all')->toArray();
         }
-    
-
 }
